7081749835cb7a7fb5e080a3b36fcf6d0b59772a
Noah <noah@arkc.org>
2015-07-23 22:02:22 +0800

    further debug object AES

--- server.py
+++ server.py
@@ -33,8 +33,8 @@ class serverreceiver(asyncore.dispatcher):
         self.from_remote_buffer = b''
         self.from_remote_buffer_raw = b''
         self.to_remote_buffer = b''
-        self.cipher1 = None
-        self.cipher2 = None
+        self.cipher = None
+        self.cipherinstance = None
         self.ctl.newconn(self)
 
     def handle_connect(self):
@@ -42,7 +42,7 @@ class serverreceiver(asyncore.dispatcher):
 
     def handle_read(self):
         read = b''
-        if self.cipher1 == None:
+        if self.cipher == None:
             try:
                 read += self.recv(768)
                 if len(read) >= 768:
@@ -53,8 +53,8 @@ class serverreceiver(asyncore.dispatcher):
                         self.ctl.closeconn()
                         self.close()
                     else:
-                        self.cipher1 = AES.new(self.ctl.localcert.decrypt(read[-256:]), AES.MODE_CFB, bytes(self.ctl.str, "UTF-8"))
-                        self.cipher2 = AES.new(self.ctl.localcert.decrypt(read[-256:]), AES.MODE_CFB, bytes(self.ctl.str, "UTF-8"))
+                        self.cipher = AES.new(self.ctl.localcert.decrypt(read[-256:]), AES.MODE_CFB, bytes(self.ctl.str, "UTF-8"))
+                        self.cipherinstance = self.cipher
             except Exception as err:
                 print("Authentication failed, socket closing")
                 self.ctl.closeconn()
@@ -65,7 +65,8 @@ class serverreceiver(asyncore.dispatcher):
             bytessplit = self.from_remote_buffer_raw.split(bytes(SPLITCHAR, "UTF-8"))
             for Index in range(len(bytessplit)):
                 if Index < len(bytessplit):
-                    decryptedtext = self.cipher1.decrypt(bytessplit[Index])
+                    decryptedtext = self.cipherinstance.decrypt(bytessplit[Index])
+                    self.cipherinstance = self.cipher
                     self.from_remote_buffer += decryptedtext
                     read_count += len(decryptedtext)
                 else:
@@ -78,10 +79,11 @@ class serverreceiver(asyncore.dispatcher):
     def handle_write(self):
         if len(self.to_remote_buffer)<=4096:
             sent = len(self.to_remote_buffer)
-            self.send(self.cipher2.encrypt(self.to_remote_buffer) + bytes(SPLITCHAR, "UTF-8"))
+            self.send(self.cipherinstance.encrypt(self.to_remote_buffer) + bytes(SPLITCHAR, "UTF-8"))
         else:
-            self.send(self.cipher2.encrypt(self.to_remote_buffer[:4096]) + bytes(SPLITCHAR, "UTF-8"))
+            self.send(self.cipherinstance.encrypt(self.to_remote_buffer[:4096]) + bytes(SPLITCHAR, "UTF-8"))
             sent = 4096
+        self.cipherinstance = self.cipher
         print('%04i to server' % sent)
         self.to_remote_buffer = self.to_remote_buffer[sent:]
 
