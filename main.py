a25e31ac0e158e0c50b6112a8b4a68cf5d009d0e
Teba <teba@arkc.org>
2015-12-03 09:20:13 +0800

    Fix mismatch in defaults

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ DEFAULT_REMOTE_HOST = "0.0.0.0"
 DEFAULT_LOCAL_PORT = 8001
 DEFAULT_REMOTE_PORT = 8000
 
-DEFAULT_REQUIRED = 4                
+DEFAULT_REQUIRED = 3
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="ArkC Client")
@@ -31,42 +31,42 @@ if __name__ == '__main__':
         parser.add_argument('-c', '--config', dest="config", help="You must specify a configuration files. By default ./config.json is used.", default='config.json')
         parser.add_argument('-fs', '--frequent-swap', dest="fs", action="store_true", help="Use frequent connection swapping")  # #TODO: support this function
         options = parser.parse_args()
-        
+
         data = {}
-                
+
         # Load json configuration file
         try:
-            data_file = open(options.config)    
+            data_file = open(options.config)
             data = json.load(data_file)
             data_file.close()
         except Exception as err:
             logging.error("Fatal error while loading configuration file.\n" + err)
             quit()
-        
+
         if "control_domain" not in data:
             logging.error("missing control domain")
             quit()
-        
+
         # Apply default values
         if "local_host" not in data:
             data["local_host"] = DEFAULT_LOCAL_HOST
-            
+
         if "local_port" not in data:
             data["local_port"] = DEFAULT_LOCAL_PORT
-            
+
         if "remote_host"not in data:
             data["remote_host"] = DEFAULT_REMOTE_HOST
-            
+
         if "remote_port"not in data:
             data["remote_port"] = DEFAULT_REMOTE_PORT
-            
+
         if "number"not in data:
             data["number"] = DEFAULT_REQUIRED
-            
-        
+
+
         # Load certificates
         try:
-            remote_cert_file = open(data["remote_cert"], "r")            
+            remote_cert_file = open(data["remote_cert"], "r")
             remotecert = certloader(remote_cert_file).importKey()
             remote_cert_file.close()
         except KeyError as e:
@@ -76,7 +76,7 @@ if __name__ == '__main__':
             print ("Fatal error while loading remote host certificate.")
             print (err)
             quit()
-            
+
         try:
             local_cert_file = open(data["local_cert"], "r")
             localcert = certloader(local_cert_file).importKey()
@@ -91,7 +91,7 @@ if __name__ == '__main__':
             print ("Fatal error while loading local certificate.")
             print (err)
             quit()
-            
+
         try:
             local_pub_file = open(data["local_cert_pub"], "r")
             localpub = certloader(local_pub_file).getSHA1()
@@ -103,19 +103,19 @@ if __name__ == '__main__':
             print ("Fatal error while calculating SHA1 digest.")
             print (err)
             quit()
-            
+
         if options.v:
             logging.basicConfig(level=logging.INFO)
-            
+
         if options.fs:
             swapfq = 3
         else:
             swapfq = 8
-            
+
     except Exception as e:
         print ("An error occurred: \n")
         print(e)
-    
+
     # Start the main event loop
     try:
         ctl = coordinate(
@@ -138,12 +138,12 @@ if __name__ == '__main__':
             data["local_host"],
             data["local_port"]
             )
-    
+
     except KeyError as e:
         print(e)
         logging.error("Bad config file. Quitting.")
         quit()
-    
+
     except Exception as e:
         print ("An error occurred: \n")
         print(e)
