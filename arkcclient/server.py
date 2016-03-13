721fd8ef44a57328e8ffe6b82febd8e76f2efd3d
Tsre <tsre@arkc.org>
2016-03-13 21:56:57 +0800

    debug

--- arkcclient/server.py
+++ arkcclient/server.py
@@ -148,7 +148,8 @@ class ServerReceiver(asyncore.dispatcher):
                             cli_id = b_dec[1:3].decode("UTF-8")
                             seq = int(b_dec[3:9].decode("UTF-8"))
                             send_time = int(b_dec[9:17].decode("UTF-8"), 36)
-                            self.send_recv_time += time() - send_time / 100
+                            self.send_recv_time += time.time() - \
+                                send_time / 100
                             b_data = b_dec[17:]
                         except Exception:
                             logging.warning("decode error")
